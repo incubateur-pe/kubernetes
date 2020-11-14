0c97fd3f86de0100fb8d8f754f225984cc6c62c0
ulrich giraud <ulrich.giraud1@gmail.com>
2020-11-14 17:52:26 +0100

    feat(ci): manages multiple environements

---
+++ molecule/commun/env_ci_pe.sh
@@ -0,0 +1,7 @@
+DOCKER_REGISTRY_MIRROR=https://docker-dev-virtual.repository.pole-emploi.intra
+QUAY_REGISTRY_MIRROR=docker-quay-proxy.repository.pole-emploi.intra
+K8S_REGISTRY_MIRROR=docker-k8s-gcr-proxy.repository.pole-emploi.intra
+INSECURE_REGISTRIES=["docker-dev-virtual.repository.pole-emploi.intra", "docker-quay-proxy.repository.pole-emploi.intra", "docker-k8s-gcr-proxy.repository.pole-emploi.intra"]
+CENTOS_BASE_MIRROR=https://repository.pole-emploi.intra/artifactory/rpm-base-centos-proxy
+DOCKER_YUM_MIRROR=https://repository.pole-emploi.intra/artifactory/rpm-docker-centos-proxy
+K8S_YUM_MIRROR=https://repository.pole-emploi.intra/artifactory/rpm-kubernetes-proxy/
